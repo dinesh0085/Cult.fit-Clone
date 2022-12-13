885565d0cd02415a4fcbaa9312c8cbba520e177d
1Gireesh <gireeshbhat68@gmail.com>
2022-12-13 18:43:51 +0530

    added dprice

--- backend/src/routes/product/productRoute.ts
+++ backend/src/routes/product/productRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getCare, getMind } from "../../controller/product/product";
+import { getCare, getMind, setCare, setMind } from "../../controller/product/product";
 
 export const careRouter = express.Router();
 careRouter.get("/",getCare);
+careRouter.post("/",setCare)
 
 export const mindRouter = express.Router();
 mindRouter.get("/",getMind);
-
+mindRouter.post("/",setMind);
