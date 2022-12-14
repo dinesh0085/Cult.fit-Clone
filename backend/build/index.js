0e64426507f7637dce7c61d047b909460d4a2029
1Gireesh <gireeshbhat68@gmail.com>
2022-12-14 19:48:22 +0530

    added care/id and mind/id

--- backend/build/index.js
+++ backend/build/index.js
@@ -6,14 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 require("dotenv/config");
+const mongoose_1 = __importDefault(require("mongoose"));
+const productRoute_1 = require("./routes/product/productRoute");
+const mongoUrl = process.env.MONGOURL || "";
 const port = process.env.PORT || 8080;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-function mid(req, res) {
-    res.send("app succesfully");
-}
-app.get("/", mid);
-app.listen(port, () => {
-    console.log("http://localhost:8080/");
+app.all("/", (req, res) => res.send("working"));
+app.use("/care", productRoute_1.careRouter);
+app.use("/mind", productRoute_1.mindRouter);
+app.use("/docter", productRoute_1.docterRouter);
+mongoose_1.default.connect(mongoUrl).then(() => {
+    app.listen(port, () => {
+        console.log("http://localhost:8080/");
+    });
 });
