e2a9d35a72e3b249d9532de70d319ebb908e4752
Nipendra Soni <rsoni2843@gmail.com>
2022-12-15 21:54:14 +0530

    day-3 work update

--- backend/src/routes/user/userRoutes.ts
+++ backend/src/routes/user/userRoutes.ts
@@ -2,10 +2,14 @@ import express, { Request, Response } from "express";
 import LoginController from "../../controller/user/loginController";
 const router = express.Router();
 import UserController from "../../controller/user/signupController";
+import checkUserAuth from "../../middlewares/user/userMiddleware";
 import SignupController from "./../../controller/user/signupController";
 
-// Public Routes
+// Middleware for protected routes
+router.use("/changepassword", checkUserAuth);
+router.use("/loggedUser", checkUserAuth);
 
+// Public Routes
 router.get("/", (req: Request, res: Response) => {
   res.send("User Routes Working");
 });
@@ -14,5 +18,6 @@ router.post("/login", LoginController.userLogin);
 
 // Protected Routes
 router.post("/changePassword", LoginController.changeUserPassword);
+router.get("/loggedUser", LoginController.loggedUser);
 
 export default router;
