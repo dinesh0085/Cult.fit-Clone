a286929dd4b78010b126056f70623bbb80d3705f
Dinesh_Solanki_DEV <105920688+dinesh0085@users.noreply.github.com>
2022-12-16 00:48:49 +0530

    Delete backend directory

--- backend/src/modules/product/mindFull.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const mindfullModel = mongoose.model("mindfull", new mongoose.Schema({
-    image: String,
-    title: String,
-    desc: String,
-    url: String,
-    packs: Number
-}));
--- backend/src/modules/product/mindModel.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const mindModel = mongoose.model("mind", new mongoose.Schema({
-    image: String,
-    title: String,
-    desc: String,
-    price: Number,
-}));
