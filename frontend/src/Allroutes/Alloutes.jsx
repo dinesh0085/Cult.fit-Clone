08d268c144cfd59035fac00e8d1810d62e4e26af
er-priyanka <shahanepriyankas01@gmail.com>
2022-12-17 16:09:17 +0530

    added

--- frontend/src/Allroutes/Alloutes.jsx
+++ frontend/src/Allroutes/Alloutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home } from '../Pages/Home/Home'
+// import { Home } from '../Pages/Home/Home'
+import {Home} from "../pages/Home/Home";
 
 export const Allroutes = () => {
   return (
