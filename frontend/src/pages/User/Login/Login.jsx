aa3cc7bf5f90cb8eab46faf6f15f825a9df506b3
Ashutosh Sharma <104469510+mr-ashu@users.noreply.github.com>
2022-12-17 13:57:19 +0530

    some-css

--- frontend/src/pages/User/Login/Login.jsx
+++ frontend/src/pages/User/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
-import LoginModal from "../../../components/UserComponent/Login/LoginModal";
+import LoginModal from "../../../Components/UserComponent/Login/LoginModal";
 
 const Login = () => {
   return (
