33b5f01fb28336fd02e8e43259aa468fb267dd9f
Nipendra Soni <rsoni2843@gmail.com>
2022-12-17 17:15:16 +0530

    day-5 updated

--- frontend/src/Allroutes/Alloutes.jsx
+++ frontend/src/Allroutes/Alloutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home } from '../Pages/Home/Home'
+// import { Home } from '../Pages/Home/Home'
+import {Home} from "../Pages/Home/Home";
 
 export const Allroutes = () => {
   return (
