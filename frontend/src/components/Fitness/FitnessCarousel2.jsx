538d92137a99cc52e29d641fb17c95be30bd0e9a
er-priyanka <shahanepriyankas01@gmail.com>
2022-12-18 18:06:29 +0530

    Added carousels and other contents of fitness page

--- frontend/src/components/Fitness/FitnessCarousel2.jsx
+++ frontend/src/components/Fitness/FitnessCarousel2.jsx
@@ -1,6 +1,6 @@
-import { Box, Button, Center, Divider, Flex, Heading, HStack, Icon, Image, List, Stack, Text } from "@chakra-ui/react"
-import {Carousel} from "react-responsive-carousel";
+import { Box, Button, Center, Divider, Flex, Heading, HStack, Icon, Image, Stack, Text } from "@chakra-ui/react"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Slider from "react-slick";
 
 const content = [
     {
@@ -20,7 +20,7 @@ const content = [
         img: "https://cdn-images.cure.fit/www-curefit-com/image/upload/w_300,fl_progressive,f_auto,q_auto:eco/dpr_2/image/gymfit/centers/prod/CENTER153/f5e2db6a-1992-43d6-bbef-1a57118a1946_DSC2789-HDR.JPG"
     },
     {
-        title: "MULTIFIT(GRAND TAMANNA)",
+        title: "MULTIFIT (GRAND TAMANNA)",
         location: "Hinjewadi",
         type: "Gym",
         price:"1035",
@@ -39,48 +39,86 @@ const content = [
 ]
 
 export const FitnessCarousel2 = () => {
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        initialSlide:0,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      };
+
     return (
         <Box w="80%" mt="40px">
-            <Carousel width="70%">
+            <Slider {...settings}>
                 {
                     content.map((item, i) => (
-                        <Box key={i} >
+                        <Box p="10px" key={i} >
                             <Box>
-                                <Image w="50%" h="250px" src={item.img} />
+                                <Image borderRadius="20px 20px 0 0" w="100%" h="250px" src={item.img} />
                             </Box>
-                            <Flex p="20px" mt="10px" justifyContent="space-between">
-                                <Stack>
-                                    <Heading>{item.title}</Heading>
-                                    <HStack>
-                                        <Text>{item.location}</Text>
-                                        {
-                                            (i!==content.length-1)
-                                            ?(
-                                                <Center>
-                                                    <Icon fontSize="0.5rem" viewBox='0 0 200 200'>
-                                                        <path
-                                                            fill='white'
-                                                            d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
-                                                        />
-                                                    </Icon>
-                                                </Center>
-                                            ) : <Text as={'span'}></Text>
-                                        }
-                                        
-                                        
-                                        <Text>{item.type}</Text>
-                                    </HStack>
-                                </Stack>
-                                <Box>
-                                    <Button bgColor="gray.600">{item.buttonText}</Button>
-                                </Box>
-                            </Flex>
-                            {(i !== content.length-1) ? <Divider /> : <Text></Text>}
-                            <Text m="20px" textAlign="left">{(i!==content.length-1) ? `Unlimited access starting at Rs. ${item.price}` : " "}</Text>
+                            <Box minH="180px" bgColor="gray.600" pb="5px" borderRadius="0 0 20px 20px">
+                                <Flex p="20px" mt="10px" justifyContent="space-between">
+                                    <Stack textAlign="left">
+                                        <Heading noOfLines={1} fontSize="4xl">{item.title}</Heading>
+                                        <HStack>
+                                            <Text>{item.location}</Text>
+                                            {
+                                                (i!==content.length-1)
+                                                ?(
+                                                    <Center>
+                                                        <Icon fontSize="0.5rem" viewBox='0 0 200 200'>
+                                                            <path
+                                                                fill='white'
+                                                                d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
+                                                            />
+                                                        </Icon>
+                                                    </Center>
+                                                ) : <Text as={'span'}></Text>
+                                            }
+                                            
+                                            
+                                            <Text>{item.type}</Text>
+                                        </HStack>
+                                    </Stack>
+                                    <Box>
+                                        <Button bgColor="gray.600">{item.buttonText}</Button>
+                                    </Box>
+                                </Flex>
+                                {(i !== content.length-1) ? <Divider /> : <Text></Text>}
+                                <Text m="20px" textAlign="left">{(i!==content.length-1) ? `Unlimited access starting at Rs. ${item.price}` : " "}</Text>
+                            </Box>
+                            
                         </Box>
                     ))
                 }
-            </Carousel>
+            </Slider>
         </Box>
     )
 }
