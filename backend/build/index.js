1c43276d9ea57bc48248e4130ca1a91089444ba0
Nipendra Soni <rsoni2843@gmail.com>
2022-12-19 09:07:03 +0530

    changes done

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -18,7 +20,7 @@ app.use("/care", productRoute_1.careRouter);
 app.use("/mind", productRoute_1.mindRouter);
 app.use("/docter", productRoute_1.docterRouter);
 mongoose_1.default.connect(mongoUrl).then(() => {
-    app.listen(port, () => {
-        console.log("http://localhost:8080/");
-    });
+  app.listen(port, () => {
+    console.log("http://localhost:8080/");
+  });
 });
